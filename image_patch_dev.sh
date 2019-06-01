69f52cdd06ced3a8ee25408317e707bb8a1e1272
Kumar Abhishek <kr.abhishek8260@gmail.com>
2019-05-31 21:03:44 -0700

    Add echo for IMG

--- image_patch_dev.sh
+++ image_patch_dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 IMG=$(ko resolve -f config/default/manager/manager.yaml | grep 'image:' | awk '{print $2}')
+echo "$IMG"
 if [ -z ${IMG} ]; then exit; fi
 cat > config/overlays/development/manager_image_patch.yaml << EOF 
 apiVersion: apps/v1
